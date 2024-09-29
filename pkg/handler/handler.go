b866888293cd55f7092d525c9e2db818450e06a6
Screxy <dvbvladis@mail.ru>
2024-09-29 21:52:48 +0300

    feat: hot-reload golang image

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/goGo-service/back/pkg/service"
+	"time"
 )
 
 type Handler struct {
@@ -15,7 +17,13 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
-
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"http://localhost:5173", "https://stallion-new-infinitely.ngrok-free.app"},
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		ExposeHeaders: []string{"Content-Length"},
+		MaxAge:        12 * time.Hour,
+	}))
 	auth := router.Group("/auth")
 	{
 		auth.POST("/sign-up", h.signUp)
