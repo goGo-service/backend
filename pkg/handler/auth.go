e0426a9e8bf6a5c3d16ffcd9fe5559440488ceff
Screxy <dvbvladis@mail.ru>
2024-09-30 21:55:35 +0300

    feat: jwt cors

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"github.com/golang-jwt/jwt/v5"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	goGO "github.com/goGo-service/back"
@@ -26,6 +28,32 @@ func (h *Handler) signUp(c *gin.Context) {
 	})
 }
 
-func (h *Handler) signIn(c *gin.Context) {
+type tokenClaims struct {
+	jwt.RegisteredClaims
+	UserId int `json:"user_id"`
+}
 
+func (h *Handler) signIn(c *gin.Context) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+		1,
+	})
+	ss, _ := token.SignedString([]byte("AllYourBase"))
+	http.SetCookie(c.Writer, &http.Cookie{
+		Name:     "refresh_token",
+		Value:    "tesst",
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: false,
+		Domain:   "stallion-new-infinitely.ngrok-free.app",
+		//SameSite: http.SameSiteNoneMode,
+		//Secure:   true,
+	})
+	c.JSON(200, gin.H{
+		"action":       "auth",
+		"access_token": ss,
+	})
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -18,17 +18,31 @@ func NewHandler(services *service.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:  []string{"http://localhost:5173", "https://stallion-new-infinitely.ngrok-free.app"},
-		AllowMethods:  []string{"*"},
-		AllowHeaders:  []string{"*"},
-		ExposeHeaders: []string{"Content-Length"},
-		MaxAge:        12 * time.Hour,
+		AllowOrigins:     []string{"http://localhost:5173", "https://stallion-new-infinitely.ngrok-free.app"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Явно перечисляем методы
+		AllowHeaders:     []string{"Content-Type", "Authorization"},           // Явно перечисляем заголовки
+		ExposeHeaders:    []string{"Content-Length"},
+		MaxAge:           12 * time.Hour,
+		AllowCredentials: true,
 	}))
 	auth := router.Group("/auth")
 	{
 		auth.POST("/sign-up", h.signUp)
 		auth.POST("/sign-in", h.signIn)
+		auth.GET("/logout", func(c *gin.Context) {
+			c.JSON(200, "")
+		})
 	}
+
+	router.GET("/profile", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"id":         1337,
+			"username":   "screxy",
+			"last_name":  "Миронов",
+			"first_name": "Владислав",
+			"email":      "dvbvladis@mail.ru",
+		})
+	})
 
 	return router
 }
